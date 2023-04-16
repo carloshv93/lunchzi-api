4dde8377cbd1d52df1ebcf484742faeb7b24259a
CARLOS-PC\Carlos <carloshv93@gmail.com>
2023-04-16 16:32:50 -0600

    feat(api): add hello world response

---
+++ api/index.ts
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("*", (req, res) => {
+  res.send("Hello World!");
+});
+
+module.exports = app;
